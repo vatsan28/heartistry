be2d2423d1312e29e512e940b98c45744da7a611
Srivatsan Muralidharan <srivatsan.muralidharan@grainger.com>
2019-07-01 17:26:38 -0500

    Add routing for actual layout

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,14 @@
 import React from "react";
-import  { HashRouter, Switch, Route } from 'react-router-dom';
+import Home from "./Home";
+import  { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Router = () => {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <Switch>
-                <Route exact path = "/" render={() => {window.location.href="./ComingSoon/index.html"}} />
+                <Route path = "/" component={Home} />
             </Switch>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
